40a39985f2312b1c53b56e98bb827009080a4c75
VoskresenskayaM <vmm459@gmail.com>
2024-03-18 17:47:11 +0500

    feat: add mobile version

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,41 +1,65 @@
 import style from './Header.module.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ReactComponent as Burger } from '../../assets/images/burger.svg';
 import { ReactComponent as Logo } from '../../assets/images/logo.svg';
 import { ReactComponent as City } from '../../assets/images/city.svg';
 import { ReactComponent as Search } from '../../assets/images/search.svg';
+import { ReactComponent as Profole } from '../../assets/images/profile.svg';
+import { useWindowSize } from '../../hooks/index';
 
 export const Header = () => {
+    const size = useWindowSize();
+    useEffect(() => {}, [size]);
     const handlerClick = () => {};
     return (
-        <header className={style.header}>
-            <div className={style.header__logo}>
-                <button
-                    className={style.header__burgerButton}
-                    onClick={handlerClick}
-                >
-                    <Burger />
-                </button>
-                <Logo />
-            </div>
-            <form className={style.headerForm}>
-                <button type="submit" className={style.headerForm__button}>
-                    <Search />
-                </button>
-                <input
-                    type="text"
-                    placeholder="Поиск"
-                    className={style.headerForm__input}
-                />
-            </form>
-
-            <div className={style.header__cityContainer}>
-                <div className={style.header__city}>
-                    <City className={style.header__cityArrow} />
-                    <p className={style.header__cityText}>Санкт-Петербург</p>
-                </div>
-                <button className={style.header__signin}>ВОЙТИ</button>
-            </div>
-        </header>
+        <>
+            {size.width > 1100 ? (
+                <header className={style.header}>
+                    <div className={style.header__logo}>
+                        <button
+                            className={style.header__burgerButton}
+                            onClick={handlerClick}
+                        >
+                            <Burger />
+                        </button>
+                        <Logo className={style.header__logoImg} />
+                    </div>
+                    <form className={style.headerForm}>
+                        <button
+                            type="submit"
+                            className={style.headerForm__button}
+                        >
+                            <Search />
+                        </button>
+                        <input
+                            type="text"
+                            placeholder="Поиск"
+                            className={style.headerForm__input}
+                        />
+                    </form>
+                    <div className={style.header__cityContainer}>
+                        <div className={style.header__city}>
+                            <City className={style.header__cityArrow} />
+                            <p className={style.header__cityText}>
+                                Санкт-Петербург
+                            </p>
+                        </div>
+                        <button className={style.header__signin}>ВОЙТИ</button>
+                    </div>
+                </header>
+            ) : (
+                <header className={`${style.header} ${style.header__mobile}`}>
+                    {' '}
+                    <button
+                        className={style.header__burgerButton}
+                        onClick={handlerClick}
+                    >
+                        <Burger />
+                    </button>
+                    <Logo className={style.header__logoImg} />
+                    <Profole />
+                </header>
+            )}
+        </>
     );
 };
